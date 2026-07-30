25187f4e1c0314543da0355bbd5ac7fbf09f3003
anurag-17 <anurag.pandey17@gmail.com>
2026-07-30 14:48:03 +0530

    layout fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: `${APP_NAME} — Office Task Management`,
+  title: `${APP_NAME} `,
   description: APP_DESCRIPTION,
 };
 
